d9c3168666d850a530d4d0426c11e69a1f9236f2
Vinicius Kremer Santos <vinicius.kremer@gmail.com>
2023-04-24 20:54:32 -0300

    [RELEASE] Adicionando firebase

---
+++ src/app/services/firebase.service.ts
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyA_fy0xCRjmzxvFDs_JZ6ME8y4K_dYPpHU',
+  authDomain: 'alegriatech-2bf22.firebaseapp.com',
+  projectId: 'alegriatech-2bf22',
+  storageBucket: 'alegriatech-2bf22.appspot.com',
+  messagingSenderId: '109146808970',
+  appId: '1:109146808970:web:250ad196a46c2b6dff0373',
+  measurementId: 'G-5XBELVFYJX',
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
