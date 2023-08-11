7905d7b6a8ae58d4d3e050e05639bd7931fc7d72
Vinicius Kremer Santos <vinicius.kremer@gmail.com>
2023-08-10 22:08:15 -0300

    [AD-22] Adicionando script python

---
+++ .github/scripts/send_sftp/__main__.py
@@ -0,0 +1,15 @@
+import argparse
+import pysftp
+
+def main(params):
+    print(params)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Parametros para execução do dataflow stream')
+    parser.add_argument('host', help='Host para realizar a conexão.')
+    parser.add_argument('user', help='Usuário para realizar a conexão.')
+    parser.add_argument('pass', help='Senha para realizar a conexão.')
+    parser.add_argument('file_path', help='Arquivo que deve ser transferido.')
+    params = vars(parser.parse_args())
+
+    main(params)
