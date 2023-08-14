f21da70d3b82eb0f97debcedf0cca367fdf06c96
Vinicius Kremer Santos <vinicius.kremer@gmail.com>
2023-08-14 20:41:54 -0300

    [AD-22] Aprimorando script SFTP

--- .github/scripts/send_sftp/__main__.py
+++ .github/scripts/send_sftp/__main__.py
@@ -1,15 +1,29 @@
 import argparse
 import pysftp
 
+
+ALEGRIA_PATH_FOLDER = "/home/storage/c/93/dd/concafras1/public_html/alegriacrista"
+HOST_PORT = 22
+
+
 def main(params):
+  # Configurações necessárias para não verificar as chaves SSH's
+  cnopts = pysftp.CnOpts()
+  cnopts.hostkeys = None
+
+  # Início da conexão com o servidor da CONCAFRAS
+  with pysftp.Connection(host=params['host'], username=params['user'], password=params['pass'], port=HOST_PORT, cnopts=cnopts, log="./log_script.log") as sftp:
     print(params)
+    with sftp.cd(f"{ALEGRIA_PATH_FOLDER}"):
+      print("Entrou")
+
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Parametros para execução do dataflow stream')
-    parser.add_argument('host', help='Host para realizar a conexão.')
-    parser.add_argument('user', help='Usuário para realizar a conexão.')
-    parser.add_argument('pass', help='Senha para realizar a conexão.')
-    parser.add_argument('file_path', help='Arquivo que deve ser transferido.')
-    params = vars(parser.parse_args())
+  parser = argparse.ArgumentParser(description='Parametros para execução do dataflow stream')
+  parser.add_argument('host', help='Host para realizar a conexão.')
+  parser.add_argument('user', help='Usuário para realizar a conexão.')
+  parser.add_argument('pass', help='Senha para realizar a conexão.')
+  parser.add_argument('file_path', help='Arquivo que deve ser transferido.')
+  params = vars(parser.parse_args())
 
-    main(params)
+  main(params)
