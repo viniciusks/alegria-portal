ef64d66f89262a35446a86520ba7bf0f2bd4e4c2
Vinicius Kremer Santos <vinicius.kremer@gmail.com>
2023-08-21 09:39:58 -0300

    [RELEASE] Atualização do script

--- .github/scripts/send_sftp/__main__.py
+++ .github/scripts/send_sftp/__main__.py
@@ -2,18 +2,34 @@ import argparse
 import pysftp
 
 
-ALEGRIA_PATH_FOLDER = "/home/storage/c/93/dd/concafras1/public_html/alegriacrista"
+ALEGRIA_PATH_FOLDER = "/home/storage/c/93/dd/concafras1/public_html"
 HOST_PORT = 22
+
+
+def find_and_replace_in_file(env_path):
+  # Lendo arquivo
+  with open('dist/alegria-portal/index.html', 'r') as file :
+    filedata = file.read()
+
+  # Reescrevendo o texto específico
+  filedata = filedata.replace('/alegriacrista', env_path)
+
+  # Escrevendo a saída
+  with open('dist/alegria-portal/index.html', 'w') as file:
+    file.write(filedata)
 
 
 def main(params):
   # Configurações necessárias para não verificar as chaves SSH's
   cnopts = pysftp.CnOpts()
   cnopts.hostkeys = None
+  env_path = "/alegriacrista/" if params['env'] == 'prd' else "/alegriacrista-dev/"
+
+  # Reescrevendo a base da conexão
+  find_and_replace_in_file(env_path)
 
   # Início da conexão com o servidor da CONCAFRAS
   with pysftp.Connection(host=params['host'], username=params['user'], password=params['pass'], port=HOST_PORT, cnopts=cnopts, log="./log_script.log") as sftp:
-    env_path = "/" if params['env'] == 'prd' else "/dev"
     absolute_path = f"{ALEGRIA_PATH_FOLDER}{env_path}"
     with sftp.cd(absolute_path):
       print(f"Entrou dentro do {absolute_path}")
