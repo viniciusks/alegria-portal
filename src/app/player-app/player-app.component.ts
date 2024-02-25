932d495cd4fe8d6fdf3728aad73a5b76ca0c4f57
Vinicius Kremer Santos <vinicius.kremer@gmail.com>
2024-02-25 09:57:27 -0300

    [AD-33] Adicionando música através da api

--- src/app/player-app/player-app.component.ts
+++ src/app/player-app/player-app.component.ts
@@ -1,12 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { Music } from '../models/music';
+import { AlbumService } from '../services/album.service';
 
 @Component({
   selector: 'app-player-app',
   templateUrl: './player-app.component.html',
   styleUrls: ['./player-app.component.css'],
+  providers: [AlbumService],
 })
 export class PlayerAppComponent implements OnInit {
-  constructor() {}
+  music: Music;
 
-  ngOnInit(): void {}
+  constructor(private _albumService: AlbumService) {
+    this.music = {
+      title: '',
+      artist: '',
+      cover: '',
+      file: '',
+    };
+  }
+
+  ngOnInit(): void {
+    this.getAlbums();
+  }
+
+  getAlbums() {
+    this._albumService.getAlbums().subscribe((response: any) => {
+      this.music = response.data[0].album.musics[0];
+    });
+  }
 }
