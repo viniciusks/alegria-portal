ff9f60bb0cbf28c0cbbe27011d7d0ca3031279f8
Vinicius Kremer Santos <vinicius.kremer@gmail.com>
2024-02-26 21:33:01 -0300

    [AD-33] Adicionando função de play

--- src/app/player-app/player-app.component.ts
+++ src/app/player-app/player-app.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Music } from '../models/music';
 import { AlbumService } from '../services/album.service';
+import { Album } from '../models/album';
 
 @Component({
   selector: 'app-player-app',
@@ -9,7 +10,12 @@ import { AlbumService } from '../services/album.service';
   providers: [AlbumService],
 })
 export class PlayerAppComponent implements OnInit {
+  @ViewChild('audioPlayer')
+  audioPlayerRef!: ElementRef;
   music: Music;
+  albums: any[];
+  currentAlbum: Album;
+  index: any;
 
   constructor(private _albumService: AlbumService) {
     this.music = {
@@ -17,6 +23,14 @@ export class PlayerAppComponent implements OnInit {
       artist: '',
       cover: '',
       file: '',
+    };
+    this.albums = [];
+    this.index = 0;
+    this.currentAlbum = {
+      name: '',
+      owner: '',
+      musics: [],
+      link: '',
     };
   }
 
@@ -26,7 +40,31 @@ export class PlayerAppComponent implements OnInit {
 
   getAlbums() {
     this._albumService.getAlbums().subscribe((response: any) => {
-      this.music = response.data[0].album.musics[0];
+      this.albums = response.data;
+      this.currentAlbum = this.albums[0].album;
+      this.start();
     });
+  }
+
+  start() {
+    this.music = this.currentAlbum.musics[this.index];
+  }
+
+  next() {
+    this.index++;
+    this.start();
+    this.play();
+  }
+
+  play() {
+    var playPromise = this.audioPlayerRef.nativeElement.play();
+
+    if (playPromise !== undefined) {
+      playPromise
+        .then((response: any) => {})
+        .catch((error: any) => {
+          this.play();
+        });
+    }
   }
 }
